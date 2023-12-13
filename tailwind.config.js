e48f5d9d0737db871d3f10d191cc9cc4aa5218d5
arsyadal <arsyadal@student.telkomuniversity.ac.id>
2023-12-13 21:42:55 +0700

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        cads: {
+          primary: "#0F4A90",
+
+          secondary: "#133256",
+
+          accent: "#ffffff",
+
+          neutral: "#ffffff",
+
+          "base-100": "#ffffff",
+
+          info: "#ffffff",
+
+          success: "#00ffff",
+
+          warning: "#ffffff",
+
+          error: "#ffffff",
+        },
+      },
+    ],
+  },
+};
